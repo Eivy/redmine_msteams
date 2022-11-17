028131441dce59dd8dbc4a74e75d167c809cc864
haradono <haradono@triart.co.jp>
2022-11-17 18:21:18 +0900

    bump up version to 0.1.0

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_msteams do
   name 'Redmine Msteams plugin'
   author 'Eivy'
   description 'Send notification to MS Teams'
-  version '0.0.4'
+  version '0.1.0'
   url 'https://github.com/Eivy/redmine_msteams'
   author_url 'https://eivy.github.io/'
   settings :partial => 'settings/msteams_settings', :default => {'service_url' => '', 'channel_id' => '', 'client_id' => '', 'client_secret' => '', 'only_assign' => ''}
