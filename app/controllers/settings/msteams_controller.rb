2010c2b56ab7963c7355ac64882667f7cf17b52c
haradono <haradono@triart.co.jp>
2023-08-29 10:46:58 +0900

    Support Redmine 5

--- app/controllers/settings/msteams_controller.rb
+++ app/controllers/settings/msteams_controller.rb
@@ -1,6 +1,7 @@
+require File.expand_path('../../../../lib/redmine_msteams/msteams', __FILE__)
 class Settings::MsteamsController < ApplicationController
   def test
-    Msteams::Msteams.post("this is test from redmine", [User.current])
+    RedmineMsteams::Msteams::Msteams::post("this is test from redmine", [User.current])
     redirect_to request.referer
   end
 end
